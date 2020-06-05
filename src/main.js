5a57309e6da4731e44a460ea54fa5f0aaa6625b8
Florian Kittel <mail@florian-kittel.de>
2020-06-05 21:59:51 +0200

    feat(proof-of-work): add basic function

--- src/main.js
+++ src/main.js
@@ -1,73 +1,86 @@
 const SHA512 = require('crypto-js/sha512');
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.noce = 0;
+  }
+
+  calculateHash() {
+    return SHA512(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.noce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      this.noce++;
+      this.hash = this.calculateHash();
     }
 
-    calculateHash() {
-        return SHA512(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
-    }
+    console.log('Block mined:', this.hash);
+  }
 
 }
 
 class Blockhcain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()]
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, this.getTimestamp(), {
+      message: 'init'
+    }, 0)
+  }
+
+  getTimestamp() {
+    return Date.now() / 1000 | 0;
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLastBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+
+      return true;
     }
-
-    createGenesisBlock() {
-        return new Block(0, this.getTimestamp(), {
-            message: 'init'
-        }, 0)
-    }
-
-    getTimestamp() {
-        return Date.now() / 1000 | 0;
-    }
-
-    getLastBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLastBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-                return false;
-            }
-
-            return true;
-        }
-    }
+  }
 }
 
 let myCoin = new Blockhcain();
 
+console.log('Mining block 1...');
 myCoin.addBlock(new Block(1, myCoin.getTimestamp(), {
-    message: 'Block 1',
-    amout: 5
+  message: 'Block 1',
+  amout: 5
 }));
 
+console.log('Mining block 2...');
 myCoin.addBlock(new Block(2, myCoin.getTimestamp(), {
-    message: 'Block 2',
-    amout: 10
+  message: 'Block 2',
+  amout: 10
 }));
 
 console.log(JSON.stringify(myCoin, 0, 2));
