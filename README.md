c86624180e0548cc0e0a5f59f58c36a5f2abd54c
Florian Kittel <mail@florian-kittel.de>
2020-06-06 11:04:39 +0200

    feat(signiture): add wallet address mechanism

---
+++ README.md
@@ -0,0 +1,7 @@
+# Blockchain in JavaScript example
+
+This is a personal learning project to better understand blockchain and its mechnism. The initial code ist base on a tutorial by Simply Explained.
+
+**Source**
+
+[YouTube Channel Simply Explained - Building a blockchain with Javascript](https://www.youtube.com/watch?v=zVqczFZr124&list=PLzvRQMJ9HDiTqZmbtFisdXFxul5k0F-Q4)
--- src/main.js
+++ src/main.js
@@ -1,120 +1,26 @@
-const SHA512 = require('crypto-js/sha512');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction {
-  constructor(fromAddress, toAddress, amout) {
-    this.fromAddress = fromAddress;
-    this.toAddress = toAddress;
-    this.amout = amout;
-  }
-}
+const {
+  Blockhcain,
+  Transaction
+} = require('./blockchain');
 
-class Block {
-  constructor(timestamp, transactions, previousHash = '') {
-    this.timestamp = timestamp;
-    this.transactions = transactions;
-    this.previousHash = previousHash;
-    this.hash = this.calculateHash();
-    this.noce = 0;
-  }
-
-  calculateHash() {
-    return SHA512(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.noce).toString();
-  }
-
-  mineBlock(difficulty) {
-    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
-      this.noce++;
-      this.hash = this.calculateHash();
-    }
-
-    console.log('Block mined:', this.hash);
-  }
-
-}
-
-class Blockhcain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-    this.difficulty = 2;
-    this.pendingTransactions = [];
-    this.miningReward = 100;
-  }
-
-  createGenesisBlock() {
-    return new Block(this.getTimestamp(), 'init', 0)
-  }
-
-  getTimestamp() {
-    return Date.now() / 1000 | 0;
-  }
-
-  getLastBlock() {
-    return this.chain[this.chain.length - 1];
-  }
-
-  miningPendingTransactions(miningRewardAddress) {
-    let block = new Block(myCoin.getTimestamp(), this.pendingTransactions);
-    block.mineBlock(this.difficulty);
-
-    console.log('Block successfully mined!');
-    this.chain.push(block);
-
-    this.pendingTransactions = [
-      new Transaction(null, miningRewardAddress, this.miningReward)
-    ];
-  }
-
-  createTransaction(transaction) {
-    this.pendingTransactions.push(transaction);
-  }
-
-  getBalanceOfAddress(address) {
-    let balance = 0;
-
-    for (const block of this.chain) {
-      for (const trans of block.transactions) {
-        if (trans.fromAddress === address) {
-          balance -= trans.amout;
-        }
-
-        if (trans.toAddress === address) {
-          balance += trans.amout;
-        }
-      }
-    }
-
-    return balance;
-  }
-
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false;
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false;
-      }
-
-      return true;
-    }
-  }
-}
+// Example Public key for Demo
+const myKey = ec.keyFromPrivate('8e72d21d875644dd4a98f3fa665b31bd7b6413799d96b5677884626e062ecca2');
+const myWalletAddress = myKey.getPublic('hex');
 
 let myCoin = new Blockhcain();
 
-myCoin.createTransaction(new Transaction('address1', 'address2', 100));
-myCoin.createTransaction(new Transaction('address2', 'address1', 50));
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+myCoin.addTransaction(tx1);
 
 console.log('Starting the minder...');
-myCoin.miningPendingTransactions('miner-address');
+myCoin.miningPendingTransactions(myWalletAddress);
 
-console.log('Balance of miner is', myCoin.getBalanceOfAddress('miner-address'));
+console.log('Balance of miner is', myCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('Starting the minder...');
-myCoin.miningPendingTransactions('miner-address');
+console.log('Is chain valid?', myCoin.isChainValid());
 
-console.log('Balance of miner is', myCoin.getBalanceOfAddress('miner-address'));
+// console.log(JSON.stringify(myCoin, null, 4));
